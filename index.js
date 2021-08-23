7f03ee54211b2d35ace18fe89771b598334a660c
quangduy2208 <quangduy2208@gmail.com>
2021-08-23 17:35:08 +0700

    update menu NAV

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
     res.render('index', {
       name: 'AAA'
