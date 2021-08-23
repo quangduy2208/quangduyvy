eeb71abf62a1c9c953ca848eb9560f7d30641249
quangduy2208 <quangduy2208@gmail.com>
2021-08-23 11:14:51 +0700

    up trang views index

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+var express = require('express');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', function(req, res) {
+    res.render('index', {
+      name: 'AAA'
+    });
+  });
+
+  app.listen(port, '0.0.0.0', function() {
+    console.log('Server listening on port ' + port);
+  });
